6271c94809cb561ce24da5e67ceb2c3204e33191
cat-grep <huang674@wisc.edu>
2024-12-26 13:03:29 -0600

    Deploy using github action

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: "/us-national-park-visit",
+			base: process.env.NODE_ENV === 'production' ? "/us-national-park-visit": '',
 		},
 	}
 };
