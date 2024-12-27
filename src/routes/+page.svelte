708e8807c5f2f123cde5024ac6a59ec0ac19bbbc
cat-grep <huang674@wisc.edu>
2024-12-26 20:34:13 -0600

    Modify Calcite import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   // Importing the Calcite Icon component for sidebar icons
+  import { setAssetPath } from "@esri/calcite-components/dist/components";
+  // CDN hosted assets
+  setAssetPath("https://js.arcgis.com/calcite-components/2.13.2/assets");
   import "@esri/calcite-components/dist/components/calcite-icon";
   import "@esri/calcite-components/dist/calcite/calcite.css"; // Required for Calcite styles
 
